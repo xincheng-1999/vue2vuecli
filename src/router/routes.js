613e44bc8387da0784609f350aa8320068d08c1f
gaoxincheng <gaoxincheng@yolanda.hk>
2022-03-08 14:14:39 +0800

    路由懒加载

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,6 @@
 // import Home from '../pages/Home.vue'
 import FrontPage from '../pages/FrontPage.vue'
 export default  [
-  { path: '/Home', component: import(/* webpackChunkName: "Home" */ '../pages/Home.vue') },
+  { path: '/Home', component: ()=>import(/* webpackChunkName: "Home" */ '../pages/Home.vue') },
   {path:'/', component: FrontPage}
 ]
